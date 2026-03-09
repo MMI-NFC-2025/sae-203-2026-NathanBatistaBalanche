76ae4b9058f967913f3ac6f2e263f895c1e53755
NathanBatistaBalanche <nathan.batista_balanche@edu.univ-fcomte.fr>
2026-03-09 19:14:17 +0100

    Ajout images / data pocketbase

---
+++ backend/pocketbase/pb_migrations/1773059516_updated_scenes.js
@@ -0,0 +1,48 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("pbc_1254289504")
+
+  // add field
+  collection.fields.addAt(1, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text1819170229",
+    "max": 0,
+    "min": 0,
+    "name": "nom",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  // add field
+  collection.fields.addAt(2, new Field({
+    "hidden": false,
+    "id": "file347571224",
+    "maxSelect": 1,
+    "maxSize": 0,
+    "mimeTypes": [],
+    "name": "photo",
+    "presentable": false,
+    "protected": false,
+    "required": false,
+    "system": false,
+    "thumbs": [],
+    "type": "file"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_1254289504")
+
+  // remove field
+  collection.fields.removeById("text1819170229")
+
+  // remove field
+  collection.fields.removeById("file347571224")
+
+  return app.save(collection)
+})
