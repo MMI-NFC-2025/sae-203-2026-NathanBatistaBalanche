948b940704ba3470ae041e106c98490cdaaf3dfe
NathanBatistaBalanche <nathan.batista_balanche@edu.univ-fcomte.fr>
2026-03-12 15:34:26 +0100

    Dépoloiement du site et de la base de données

--- src/script/backend.mjs
+++ src/script/backend.mjs
@@ -1,5 +1,5 @@
 import PocketBase from 'pocketbase';
-const pb = new PocketBase('http://127.0.0.1:8090');
+const pb = new PocketBase('https://jumelages-pb.nathan-batista-balanche.fr');
 
 export async function getArtistesbyDate() {
     try {
